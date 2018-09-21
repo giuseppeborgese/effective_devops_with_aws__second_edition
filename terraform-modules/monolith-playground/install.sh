19ef2f2d8004d7240a997f2d1d395c6dd919acb7
Giuseppe Borgese <giuseppe.borgese@clavistechnology.com>
2018-09-21 21:13:03 +0200

    update install.sh

--- terraform-modules/monolith-playground/install.sh
+++ terraform-modules/monolith-playground/install.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 yum -y install httpd mariadb.x86_64 mariadb-server
 echo "<VirtualHost *>" > /etc/httpd/conf.d/tomcat-proxy.conf
 echo "        ProxyPass               /visits      http://localhost:8080/visits" >> /etc/httpd/conf.d/tomcat-proxy.conf
@@ -13,6 +13,6 @@ mysql -u root -e "CREATE TABLE visits (id bigint(20) NOT NULL AUTO_INCREMENT, co
 mysql -u root -e "INSERT INTO demodb.visits (count) values (0) ;"
 mysql -u root -e "CREATE USER 'monty'@'localhost' IDENTIFIED BY 'some_pass';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'monty'@'localhost' WITH GRANT OPTION;"
-runuser -l ec2-user -c 'cd /home/ec2-user ; curl -O https://raw.githubusercontent.com/giuseppeborgese/effective_devops_with_aws__second_edition/master/chapter6/demo-0.0.1-SNAPSHOT.jar'
+runuser -l ec2-user -c 'cd /home/ec2-user ; curl -O https://raw.githubusercontent.com/giuseppeborgese/effective_devops_with_aws__second_edition/master/terraform-modules/monolith-playground/demo-0.0.1-SNAPSHOT.jar'
 echo "runuser -l ec2-user -c 'export db_pass=some_pass; export db_user=monty; export db_url=jdbc:mysql://localhost:3306/demodb?createDatabaseIfNotExist=true; java -jar /home/ec2-user/demo-0.0.1-SNAPSHOT.jar' " >> /etc/rc.d/rc.local
 bash /etc/rc.d/rc.local
