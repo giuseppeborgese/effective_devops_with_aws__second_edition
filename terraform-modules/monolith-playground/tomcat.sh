23056d60cab6619f98814314294f0160eb22baaa
Giuseppe Borgese <giuseppe.borgese@clavistechnology.com>
2018-09-22 20:42:15 +0200

    added service

---
+++ terraform-modules/monolith-playground/tomcat.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+case $1 in
+ start)
+ # start script
+ runuser -l ec2-user -c 'export db_pass=some_pass; export db_user=monty; export db_url=jdbc:mysql://localhost:3306/demodb?createDatabaseIfNotExist=true; java -jar /home/ec2-user/demo-0.0.1-SNAPSHOT.jar'
+ ;;
+ stop)
+ # stop script
+ pkill java
+ ;;
+esac
+exit
